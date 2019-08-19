b1aeccad07aaf0bf44f5910472bbadb7099baa80
y785 <j7852398521@gmail.com>
2019-08-19 01:59:41 -0500

    Adding a generic not found script.

---
+++ src/main/java/moe/maple/api/script/helper/MoeNotFound.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2019, y785, http://github.com/y785
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package moe.maple.api.script.helper;
+
+import moe.maple.api.script.model.NpcScript;
+import moe.maple.api.script.model.object.NpcObject;
+
+/**
+ * A helper script for when an NPC isn't found.
+ * Meant to be overridden by the scripts package.
+ */
+public abstract class MoeNotFound extends NpcScript {
+
+    protected String expected;
+
+    public MoeNotFound() {
+        expected = "";
+    }
+
+    public void setExpected(String name) {
+        this.expected = name;
+    }
+
+    @Override
+    public int getSpeakerTemplateId() {
+        return getNpcObject().map(NpcObject::getTemplateId).orElse(9200000);
+    }
+}
