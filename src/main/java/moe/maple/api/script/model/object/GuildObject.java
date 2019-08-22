45e1e8f39552ac7e23f26bbb2bcb4562b58bb3aa
umbreon22 <sweis.m01@mymail.sxu.edu>
2019-08-22 00:16:24 -0500

    Added comments to GuildObject

--- src/main/java/moe/maple/api/script/model/object/GuildObject.java
+++ src/main/java/moe/maple/api/script/model/object/GuildObject.java
@@ -3,5 +3,22 @@ package moe.maple.api.script.model.object;
 /**
  * Created on 8/22/2019.
  */
-public class GuildObject {
+public interface GuildObject<Guild> extends ScriptObject<Guild> {
+    /** BMS Notes:
+     * system function(integer)		isGuildMember;
+     * 	system function(integer)		isGuildMaster;
+     * 	system function(integer)		isGuildSubMaster;
+     * 	system function(integer)		removeGuild( integer );
+     * 	system function(integer)		getGuildCountMax;
+     * 	system function(integer)		incGuildCountMax( integer, integer );
+     * 	system function(integer)		isCreateGuildPossible( integer );
+     * 	system function(integer)		createNewGuild( integer );
+     * 	system function(integer)		setGuildMark( integer );
+     * 	system function(integer)		isGuildMarkExist;
+     * 	system function(integer)		removeGuildMark( integer );
+     * 	system function(integer)		isGuildQuestRegistered;
+     * 	system function(integer)		canEnterGuildQuest;
+     * 	system function				clearGuildQuest;
+     * 	system function				incGuildPoint( integer );
+     */
 }
