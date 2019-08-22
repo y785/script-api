317bd46672bbdf7eead945cc6a0052d394ff9451
umbreon22 <sweis.m01@mymail.sxu.edu>
2019-08-21 23:56:06 -0500

    Some Documentation for Providers

--- src/main/java/moe/maple/api/script/model/object/data/provider/HairProvider.java
+++ src/main/java/moe/maple/api/script/model/object/data/provider/HairProvider.java
@@ -4,5 +4,5 @@ package moe.maple.api.script.model.object.data.provider;
  * @author umbreon22
  * Created on 8/21/2019.
  */
-public interface ItemProvider {
+public interface HairProvider extends DataProvider<Integer, Integer> {
 }
