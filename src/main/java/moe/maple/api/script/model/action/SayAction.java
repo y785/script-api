31c2411abdc637396a991c42a331ba54209ed6ac
umbreon22 <sweis.m01@mymail.sxu.edu>
2019-08-26 22:21:51 -0500

    Added SayAction interface

---
+++ src/main/java/moe/maple/api/script/model/action/SayAction.java
@@ -0,0 +1,10 @@
+package moe.maple.api.script.model.action;
+
+import moe.maple.api.script.model.chain.BasicActionChain;
+
+/**
+ * Created on 8/26/2019.
+ */
+public interface SayAction extends BasicActionChain {
+
+}
