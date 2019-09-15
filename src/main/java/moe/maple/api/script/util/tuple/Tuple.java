dd1ee5a6f80823de1949f66bffc40d3767299637
umbreon22 <sweis.m01@mymail.sxu.edu>
2019-09-14 22:11:37 -0500

    Added utility methods for Tuples.

--- src/main/java/moe/maple/api/script/util/tuple/Tuple.java
+++ src/main/java/moe/maple/api/script/util/tuple/Tuple.java
@@ -22,6 +22,12 @@
 
 package moe.maple.api.script.util.tuple;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.BiConsumer;
+
 public interface Tuple<L, R> {
     L left();
     R right();
@@ -29,4 +35,81 @@ public interface Tuple<L, R> {
     static <L, R> Tuple<L, R> of(L left, R right) {
         return new ImmutableTuple<>(left, right);
     }
+
+    /**
+     * Utility method to perform an action for each Tuple
+     * @param tuples An iterable Tuple collection
+     * @param action An action to perform for each Tuple
+     * @param <L> Left type
+     * @param <R> Right type
+     */
+    static <L, R> void forEach(Iterable<Tuple<L,R>> tuples, BiConsumer<? super L, ? super R> action) {
+        for(var tuple : tuples) {
+            action.accept(tuple.left(), tuple.right());
+        }
+    }
+
+    //========================================== Caution: memes below =================================================
+
+    /**
+     * Returns a list containing N tuples.
+     *
+     * @param <L> the left type
+     * @param <R> the right type
+     * @param args varargs to fill the list
+     * @return a {@code List} containing the specified elements
+     * @throws NullPointerException if either value is {@code null}
+     */
+    private static <L, R> List<Tuple<L, R>> unsafeListOf(Object... args) {
+        if ((args.length & 1) != 0) {
+            throw new InternalError("length is odd");
+        }
+        List<Tuple<L,R>> list = new ArrayList<>(args.length / 2);
+        for (int i = 0; i < args.length; i += 2) {
+            @SuppressWarnings("unchecked")
+            L left = Objects.requireNonNull((L)args[i]);
+            @SuppressWarnings("unchecked")
+            R right = Objects.requireNonNull((R)args[i+1]);
+            list.add(new ImmutableTuple<>(left, right));
+        }
+        return list;
+    }
+
+    static <L, R> List<Tuple<L, R>> listOf(L l1, R r1, L l2, R r2) {
+        return unsafeListOf(l1, r1, l2, r2);
+    }
+
+    static <L, R> List<Tuple<L, R>> listOf(L l1, R r1, L l2, R r2, L l3, R r3) {
+        return unsafeListOf(l1, r1, l2, r2, l3, r3);
+    }
+
+    static <L, R> List<Tuple<L, R>> listOf(L l1, R r1, L l2, R r2, L l3, R r3, L l4, R r4) {
+        return unsafeListOf(l1, r1, l2, r2, l3, r3, l4, r4);
+    }
+
+    static <L, R> List<Tuple<L, R>> listOf(L l1, R r1, L l2, R r2, L l3, R r3, L l4, R r4, L l5, R r5) {
+        return unsafeListOf(l1, r1, l2, r2, l3, r3, l4, r4, l5, r5);
+    }
+
+    static <L, R> List<Tuple<L, R>> listOf(L l1, R r1, L l2, R r2, L l3, R r3, L l4, R r4, L l5, R r5, L l6, R r6) {
+        return unsafeListOf(l1, r1, l2, r2, l3, r3, l4, r4, l5, r5, l6, r6);
+    }
+
+    static <L, R> List<Tuple<L, R>> listOf(L l1, R r1, L l2, R r2, L l3, R r3, L l4, R r4, L l5, R r5, L l6, R r6, L l7, R r7) {
+        return unsafeListOf(l1, r1, l2, r2, l3, r3, l4, r4, l5, r5, l6, r6, l7, r7);
+    }
+
+    static <L, R> List<Tuple<L, R>> listOf(L l1, R r1, L l2, R r2, L l3, R r3, L l4, R r4, L l5, R r5, L l6, R r6, L l7, R r7, L l8, R r8) {
+        return unsafeListOf(l1, r1, l2, r2, l3, r3, l4, r4, l5, r5, l6, r6, l7, r7, l8, r8);
+    }
+
+    static <L, R> List<Tuple<L, R>> listOf(L l1, R r1, L l2, R r2, L l3, R r3, L l4, R r4, L l5, R r5, L l6, R r6, L l7, R r7, L l8, R r8, L l9, R r9) {
+        return unsafeListOf(l1, r1, l2, r2, l3, r3, l4, r4, l5, r5, l6, r6, l7, r7, l8, r8, l9, r9);
+    }
+
+    static <L, R> List<Tuple<L, R>> listOf(L l1, R r1, L l2, R r2, L l3, R r3, L l4, R r4, L l5, R r5, L l6, R r6, L l7, R r7, L l8, R r8, L l9, R r9, L l10, R r10) {
+        return unsafeListOf(l1, r1, l2, r2, l3, r3, l4, r4, l5, r5, l6, r6, l7, r7, l8, r8, l9, r9, l10, r10);
+    }
+
+
 }
