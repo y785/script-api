7c2f140a6737a7f48e36ec25d4250fbca68d593e
y785 <j7852398521@gmail.com>
2020-03-10 21:07:11 -0500

    Added exports for api stuff.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,15 @@
 module moe.maple.script.api {
     requires slf4j.api;
+
+    exports moe.maple.api.script.helper;
+    exports moe.maple.api.script.logic;
+    exports moe.maple.api.script.model.object;
+    exports moe.maple.api.script.model.object.data.provider;
+    exports moe.maple.api.script.model.object.data.safety;
+    exports moe.maple.api.script.model.object.field;
+    exports moe.maple.api.script.model.object.user;
+    exports moe.maple.api.script.model.helper;
+    exports moe.maple.api.script.util;
+    exports moe.maple.api.script.util.triple;
+    exports moe.maple.api.script.util.tuple;
 }
